05743372517d7383f19a11e2074b1d3454c0a5f9
Jason Toups <jasontoups@Jasons-MacBook-Pro.local>
2018-03-06 19:36:30 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("Userf flipped " + cardThree);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne;
+
+cardOne = cards[0];
+cardTwo = cards [1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+console.log("user flipped " + cardOne);
+console.log("user flipped " + cardsInPlay);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again")
+	}
+}
