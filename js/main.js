a7f2f10d5e59f9e2cfb1a6b2bd9bebb19fc7a910
Jason Toups <jason.p.toups@gmail.com>
2018-03-07 17:26:52 -0500

    moved code into functions to store flipped card and check for results

--- js/main.js
+++ js/main.js
@@ -2,20 +2,24 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne;
 
-cardOne = cards[0];
-cardTwo = cards [1];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("user flipped " + cardOne);
-console.log("user flipped " + cardsInPlay);
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again")
 	}
 }
+}
+
+var flipCard = function(cardID) {
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
+
